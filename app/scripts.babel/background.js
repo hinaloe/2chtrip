62cef1c0cb4ae9f4bb4141543460f2d51d5b1417
hina <hina@hinaloe.net>
2017-10-12 21:29:23 +0900

    remvoe comment

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,12 +4,6 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.tabs.onUpdated.addListener(tabId => {
-  chrome.pageAction.show(tabId);
-});
-
-console.log('\'Allo \'Allo! Event Page for Page Action');
-
 chrome.contextMenus.create({
   title: '10桁トリップ逆引き',
   contexts: ['selection', 'link'],
